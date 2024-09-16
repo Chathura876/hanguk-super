60a83ee9fb878659cbc8d81139ef6c657db0a360
pruthiviAbishek <pruthiviabishek3gmail.com>
2024-09-16 18:22:38 +0530

    admin profile done

--- resources/views/owner/sidebar_pages/people/admin/profile.blade.php
+++ resources/views/owner/sidebar_pages/people/admin/profile.blade.php
@@ -22,55 +22,19 @@
         <div class="grid lg:grid-cols-2">
             <div class="lg:col-span-1">
                 <div class="p-6 rounded-lg border border-default-200">
-                    <img src="{{asset('Hanguk_super/assets/IMG/profile_images/ex_image.jpg')}}" alt="" class="w-50 mx-auto rounded-3xl p-1 border-2 border-dashed border-default-300 bg-default-100">
-                    <h4 class="mb-1 mt-3 text-lg text-default-900 text-center">Namal Bandara</h4>
-                    <p class="text-default-600 text-center mb-4">Admin</p>
-
-{{--                    <div class="flex items-center justify-center gap-2 mb-3">--}}
-{{--                        <button type="button" class="py-1.5 px-4 inline-flex items-center justify-center gap-2 font-semibold tracking-wide border align-middle duration-500 text-sm text-center bg-default-100 hover:bg-default-200 border-default-100 hover:border-default-200 text-default-900 rounded"><i class="ti ti-edit-circle"></i> Edit</button>--}}
-{{--                        <button type="button" class="py-1.5 px-4 inline-flex items-center justify-center gap-2 font-semibold tracking-wide border align-middle duration-500 text-sm text-center bg-red-500 hover:bg-red-600 border-red-500 hover:border-red-600 text-white rounded"><i class="ti ti-trash"></i> Delete</button>--}}
-{{--                    </div>--}}
+                    <img src="https://dummyimage.com/150x150/000/fff&text=Profile" alt="Profile Image" class="w-50 mx-auto rounded-3xl p-1 border-2 border-dashed border-default-300 bg-default-100">
+                    <h4 class="mb-1 mt-3 text-lg text-default-900 text-center">{{ $user->first_name }} {{ $user->last_name }}</h4>
+                    <p class="text-default-600 text-center mb-4">{{ $user->role ?? 'Role not set' }}</p>
 
                     <div class="text-start mt-6">
-                        <p class="text-default-600 mb-3"><b class="text-default-700">Full Name :</b> <span class="ms-2">Namal Bandara</span></p>
-
-                        <p class="text-default-600 mb-3"><b class="text-default-700">Nic :</b><span class="ms-2">807784177V</span></p>
-
-                        <p class="text-default-600 mb-3"><b class="text-default-700">Email :</b> <span class="ms-2 ">namal@gmail.com</span></p>
-
-                        <p class="text-default-600 mb-1.5"><b class="text-default-700">Username :</b> <span class="ms-2">Namal</span></p>
+                        <p class="text-default-600 mb-3"><b class="text-default-700">Full Name :</b> <span class="ms-2">{{ $user->first_name }} {{ $user->last_name }}</span></p>
+                        <p class="text-default-600 mb-3"><b class="text-default-700">Nic :</b><span class="ms-2">{{ $user->nic }}</span></p>
+                        <p class="text-default-600 mb-3"><b class="text-default-700">Email :</b> <span class="ms-2">{{ $user->email }}</span></p>
+                        <p class="text-default-600 mb-1.5"><b class="text-default-700">Username :</b> <span class="ms-2">{{ $user->user_name }}</span></p>
                     </div>
                 </div>
             </div>
-
         </div>
-{{--        <div class="flex items-center justify-center">--}}
-{{--        <div class="grid lg:grid-cols-1">--}}
-{{--            <div class="lg:col-span-1">--}}
-{{--                <div class="p-6 rounded-lg border border-default-200">--}}
-{{--                    <IMG src="{{asset('Hanguk_super/assets/IMG/profile_images/ex_image.jpg')}}" alt="" class="w-50 mx-auto rounded-3xl p-1 border-2 border-dashed border-default-300 bg-default-100">--}}
-{{--                    <h4 class="mb-1 mt-3 text-lg text-default-900 text-center">Namal Bandara</h4>--}}
-{{--                    <p class="text-default-600 text-center mb-4">Cashier</p>--}}
-
-{{--                    <div class="flex items-center justify-center gap-2 mb-3">--}}
-{{--                        <button type="button" class="py-1.5 px-4 inline-flex items-center justify-center gap-2 font-semibold tracking-wide border align-middle duration-500 text-sm text-center bg-default-100 hover:bg-default-200 border-default-100 hover:border-default-200 text-default-900 rounded"><i class="ti ti-edit-circle"></i> Edit</button>--}}
-{{--                        <button type="button" class="py-1.5 px-4 inline-flex items-center justify-center gap-2 font-semibold tracking-wide border align-middle duration-500 text-sm text-center bg-red-500 hover:bg-red-600 border-red-500 hover:border-red-600 text-white rounded"><i class="ti ti-trash"></i> Delete</button>--}}
-{{--                    </div>--}}
-
-{{--                    <div class="text-start mt-6">--}}
-{{--                        <p class="text-default-600 mb-3"><b class="text-default-700">Full Name :</b> <span class="ms-2">Namal Bandara</span></p>--}}
-
-{{--                        <p class="text-default-600 mb-3"><b class="text-default-700">Nic :</b><span class="ms-2">807784177V</span></p>--}}
-
-{{--                        <p class="text-default-600 mb-3"><b class="text-default-700">Email :</b> <span class="ms-2 ">namal@gmail.com</span></p>--}}
-
-{{--                        <p class="text-default-600 mb-1.5"><b class="text-default-700">Username :</b> <span class="ms-2">Namal</span></p>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-
-{{--        </div>--}}
-{{--        </div>--}}
     </div>
 
 @endsection
