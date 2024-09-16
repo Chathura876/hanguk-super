c348b5dc80b96e62046fd6769a219caea5365765
pruthiviAbishek <pruthiviabishek3gmail.com>
2024-09-16 16:37:55 +0530

    cheque modal

---
+++ database/migrations/2024_09_16_110103_create_cheques_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cheques', function (Blueprint $table) {
+            $table->id();
+            $table->string('number');
+            $table->string('bank');
+            $table->string('company');
+            $table->double('amount');
+            $table->date('issued_date');
+            $table->date('written_date');
+            $table->string('collect_by');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cheques');
+    }
+};
